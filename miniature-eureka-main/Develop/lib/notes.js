d4596309c28fc1b80921e7472e44d38bfac06ab7
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-11-15 23:11:35 -0500

    added filterbyquery function

--- miniature-eureka-main/Develop/lib/notes.js
+++ miniature-eureka-main/Develop/lib/notes.js
@@ -1,2 +1,20 @@
 const fs = require("fs");
 const path = require("path");
+
+function filterByQuery(query, notesArray) {
+  let newNotesArray = [];
+  let filteredResults = notesArray;
+  if (query.newNotes) {
+    if (typeof query.newNotes === "string") {
+      newNotesArray = [query.newNotes];
+    } else {
+      newNotesArray = query.newNotes;
+    }
+    console.log(newNotesArray);
+    newNotesArray.forEach((note) => {
+      filteredResults = filteredResults.filter(
+        (note) => note.newNotes.indexOf(note) !== -1
+      );
+    });
+  }
+}
