97b5889106e3dbbdb51e3b97d36d6057d2fd5d96
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-11-17 14:25:17 -0500

    added descriptive notes

--- server.js
+++ server.js
@@ -4,15 +4,31 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 3001;
 
+// API route that reads db.json file to return all saved notes as json
 app.get("/api/Develop/db", (req, res) => {
   res.json(notes);
 });
 
-// sendFile function to send html
+// sendFile function
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// this html route returns the notes.html file
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// this html route returns the index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+//this function receives the newly created note to save on the req body,
+//adds to json file, and returns new now to the client
+app.post("/notes"), (req, res) => {};
+
+//checks that the server is running properly by sending a message in the console
 app.listen(3001, () => {
   console.log("API server now on port 3001!");
 });
