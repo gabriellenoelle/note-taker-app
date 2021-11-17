eb10ef4c2894d7c2fe6f68093928a6b6393d246f
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-11-17 14:05:30 -0500

    deployed to heroku

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const { notes } = require("./miniature-eureka-main/Develop/db/db");
 const express = require("express");
 const app = express();
+const path = require("path");
+const port = process.env.PORT || 3001;
 
 app.get("/api/Develop/db", (req, res) => {
-  res.send("Hello!");
+  res.json(notes);
+});
+
+// sendFile function to send html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/notes.html"));
 });
 
 app.listen(3001, () => {
