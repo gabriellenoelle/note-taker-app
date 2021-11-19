d84ac26900ecd84fec126ea2c96a78e6b864a924
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-11-18 20:35:21 -0500

    fixed all paths

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const notes = require("./miniature-eureka-main/Develop/db/db");
-const express = require("express");
-const app = express();
-const path = require("path");
 const port = process.env.PORT || 3001;
 const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const allNotes = require("./miniature-eureka-main/Develop/db/db.json");
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/api/notes", (req, res) => {
+  res.json(allNotes.slice(1));
+  path.join(__dirname, "./miniature-eureka-main/Develop/db/db.json");
+});
 
 // API route that reads db.json file to return all saved notes as json
 app.get("/api/Develop/db", (req, res) => {
@@ -16,38 +21,6 @@ app.get("/api/Develop/db", (req, res) => {
     path.join(__dirname, "./miniature-eureka-main/Develop/db/db.json")
   );
 });
-
-//this function receives the newly created note to save on the req body,
-//adds to json file, and returns new now to the client
-app.post("api/notes", (req, res) => {
-  const notes = JSON.parse(
-    fs.readFileSync("./miniature-eureka-main/Develop/db/db.json")
-  );
-  const newNotes = req.body;
-  newNotes.id = uuid.v4();
-  notes.push(newNotes);
-  fs.writeFileSync(
-    "./miniature-eureka-main/Develop/db/db.json",
-    JSON.stringify(notes)
-  );
-  res.json(notes);
-});
-
-// this function creates a new note and stores it in db.json
-function createNewNote(body, notesArray) {
-  const note = body;
-  if (!Array.isArray(notesArray)) notesArray = [];
-  if (notesArray.length === 0) notesArray.push(0);
-  body.id = notesArray[0];
-  notesArray[0]++;
-
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
-    JSON.stringify({ notesArray }, null, 2)
-  );
-  return note;
-}
 
 // html calls
 app.get("/", (req, res) => {
@@ -69,6 +42,41 @@ app.get("*", (req, res) => {
     path.join(__dirname, "./miniature-eureka-main/Develop/public/index.html")
   );
 });
+
+// this function creates a new note and stores it in db.json
+function createNewNote(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray)) notesArray = [];
+  if (notesArray.length === 0) notesArray.push(0);
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
+
+//this function receives the newly created note to save on the req body,
+//adds to json file, and returns new now to the client
+app.post("/api/notes", (req, res) => {
+  const newNote = createNewNote(req.body, allNotes);
+  res.json(newNote);
+});
+
+// const notes = JSON.parse(
+//   fs.readFileSync("./miniature-eureka-main/Develop/db/db.json")
+// );
+//   const newNotes = req.body;
+//   notes.push(newNotes);
+//   fs.writeFileSync(
+//     "./miniature-eureka-main/Develop/db/db.json",
+//     JSON.stringify(notes)
+//   );
+//   res.json(notes);
+// });
 
 module.exports = {
   createNewNote,
